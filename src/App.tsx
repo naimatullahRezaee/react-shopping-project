909096c4a96a932a5bfda6df91f8859a5a87e4cd
naimatullahRezaee <naimatullahrezaee@gmail.com>
2025-09-10 09:40:06 +0430

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Store from "./pages/store/Store";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
